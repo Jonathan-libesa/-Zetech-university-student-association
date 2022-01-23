49bb3852dea5b49518f81caacf164d50e9df3336
JONA <jonathanlibesa@gmail.com>
2022-01-23 23:21:58 +0300

    updated

--- Site/settings.py
+++ Site/settings.py
@@ -137,16 +137,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 #https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 #STATIC_URL = '/MEDIA/'
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 #MEDIA_URL='/Main/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
 #MEDIA_ROOT = os.path.join(BASE_DIR,'MEDIA/Main')
 
-STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -175,7 +175,7 @@ AWS_STORAGE_BUCKET_NAME = 'zusa'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = "us-east-2" # your region
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 
