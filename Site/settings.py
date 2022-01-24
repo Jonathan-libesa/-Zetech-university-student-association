662ef9ae0e79b460ed178dcea89981cdaadc25f8
JONA <jonathanlibesa@gmail.com>
2022-01-24 13:45:55 +0300

    users

--- Site/settings.py
+++ Site/settings.py
@@ -28,7 +28,7 @@ MAX_UPLOAD_SIZE = "10485760"
 SECRET_KEY = "django-insecure-!je7e(18oi18$=d@+kkc!tk#w-!7-j8!4zec$e2q-f3s481(yk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
 ALLOWED_HOSTS = ['127.0.0.1','zetechstudentassociation.herokuapp.com']
 
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 STATIC_URL = '/MEDIA/'
 MEDIA_URL='/Main/'
 #STATICFILES_DIRS =[
-  #os.path.join(BASE_DIR,'static')
+#  os.path.join(BASE_DIR,'static')
 #]
 MEDIA_ROOT = os.path.join(BASE_DIR,'MEDIA/Main')
 # Default primary key field type
@@ -169,19 +169,15 @@ EMAIL_PORT = 587
 
 #S3 BUCKETS CONFIG
 
-AWS_ACCESS_KEY_ID = 'AKIARPPJDDSVU7FM4NZI'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'zusa'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = "us-east-2" # your region
-AWS_S3_SIGNATURE_VERSION = "s3v4"
+#AWS_ACCESS_KEY_ID = 'AKIARPPJDDSVU7FM4NZI'
+#AWS_SECRET_ACCESS_KEY = 'bsNrB3+Ms/uIj9VT3tDXcszHaQFTsx3d7ZSGm221'
+#AWS_STORAGE_BUCKET_NAME = 'zusa'
+#AWS_S3_FILE_OVERWRITE = False
+#AWS_DEFAULT_ACL = None
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#AWS_S3_REGION_NAME = "us-east-2" # your region
+#AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 
-#django_heroku.settings(locals())
-
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
