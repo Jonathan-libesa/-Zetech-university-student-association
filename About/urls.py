3120cb3cb4af2e1b32f8e8b51ee2a22da117752d
JONA <jonathanlibesa@gmail.com>
2022-01-25 17:22:12 +0300

    users

--- About/urls.py
+++ About/urls.py
@@ -3,7 +3,7 @@ from.import views
 #from.views import 
 
 urlpatterns = [
-    path('my/About-Zusa/',views.aboutZusa,name="about"),
+    path('aboutpagezetech',views.About_zetech,name="about"),
     #path('club/<str:pk>/participants/remove',views.removeparticipants,name="remove_follower"),
 
     ]
--- About/views.py
+++ About/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from.models import*
 # Create your views here.
-def aboutZusa(request):
+def About_zetech(request):
 	abouts=About.objects.all()
 	context={'abouts':abouts}
 	return render(request,'About/about.html',context)
