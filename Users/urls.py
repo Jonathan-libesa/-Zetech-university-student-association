b4f2d052d3f462bf2282dccac18ccc65728cfa5c
JONA <jonathanlibesa@gmail.com>
2022-01-29 00:19:19 +0300

    users

--- Users/urls.py
+++ Users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from.import views
 urlpatterns = [
    path(' student_registration/',views.studentpage,name="student_page"),
@@ -12,5 +13,9 @@ urlpatterns = [
    path('Add_post',views.ADD_USER_POST,name="add_post"),
    path('Delete_Post/<str:pk>',views.delete_post_user,name="delete_user_post"),
    path('profile_view_user', views. profile_view_user, name="user_profile"),
+   path('reset_password/',auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"),name="reset_password"),
+   path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+   path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+   path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 
    ]
